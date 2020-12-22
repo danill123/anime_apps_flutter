1c49c3963f689bf957197e8ebceb35db36839881
root <root@pop-os.localdomain>
2020-12-22 08:28:05 +0700

    update carousel & add null safety

--- lib/screens/views/TrendingAnime.dart
+++ lib/screens/views/TrendingAnime.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+// import 'dart:html'; // -> caused error on compiled
 
 import 'package:anime_apps_flutter/screens/views/helpers.dart';
 import 'package:flutter/material.dart';
@@ -19,12 +19,10 @@ class _TrendingAnimeState extends State<TrendingAnime> {
     data.then((value) {
       print(value);
     });
+    // only for debugging Rest API purpose
     */
     data = getData();
-    print(data);
   }
-
-  // print(await http.read('https://kitsu.io/api/edge/trending/anime'));
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +45,20 @@ class _TrendingAnimeState extends State<TrendingAnime> {
                 // success will return
                 return createCarouselList(snapshot.data, context);
               } else {
-                return CircularProgressIndicator();
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      CircularProgressIndicator(),
+                      new SizedBox(height: 15.0),
+                      Text("Make sure your internet connection stable"),
+                      new SizedBox(height: 5.0),
+                      Text("pastikan koneksi internet stabil")
+                    ],
+                  ),
+                );
+                // return CircularProgressIndicator();
               }
             },
           ),
@@ -66,30 +77,59 @@ class _TrendingAnimeState extends State<TrendingAnime> {
 }
 
 Widget createCarouselList(List data, BuildContext context) {
-  /*
-  return Container(
-    child: ListView.builder(
-      itemCount: data.length,
-      itemBuilder: (context, int index) {
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ListTile(
-              title: Text("${data[index]["attributes"]["canonicalTitle"]}"),
-              subtitle: Text("${data[index]["attributes"]["slug"]}"),
-            )
-          ],
-        );
-      },
-    ),
-  );
-  */
   return Container(
     child: Swiper(
       itemCount: data.length,
       itemBuilder: (context, int index) {
+        /*
         return Image.network(data[index]["attributes"]["posterImage"]["small"],
             fit: BoxFit.fitHeight);
+        */
+        return new Card(
+          elevation: 2.0,
+          shape: new RoundedRectangleBorder(
+            borderRadius: new BorderRadius.circular(16.0),
+          ),
+          child: new InkWell(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new ClipRRect(
+                  child: new Image.network(
+                    data[index]["attributes"]["posterImage"]["small"] ??
+                        'https://via.placeholder.com/400x200',
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: new Radius.circular(16.0),
+                    topRight: new Radius.circular(16.0),
+                  ),
+                ),
+                new Padding(
+                  padding: new EdgeInsets.all(16.0),
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      new Text(
+                          data[index]["attributes"]["titles"]["en_jp"] ?? ' '),
+                      new SizedBox(height: 5.0),
+                      Text(
+                          "Rating : ${data[index]["attributes"]["averageRating"] ?? ' '}"),
+                      Text(
+                          "Age rating : ${data[index]["attributes"]["ageRating"] ?? ' '}") // add text with null safety
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            onTap: () {
+              print("tested dude");
+              // Navigator.of(context).push(MaterialPageRoute(
+              //     builder: (context) => new DetailPage(data: this.data)));
+            },
+          ),
+        );
       },
       itemWidth: 400.0,
       itemHeight: 500.0,
